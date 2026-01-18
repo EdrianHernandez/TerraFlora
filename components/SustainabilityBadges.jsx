60fa5b4d565c6bcc32fd1a249e64881354f70f30
Edrian Hernandez <117151322+EdrianHernandez@users.noreply.github.com>
2026-01-18 22:31:08 +0800

    Rename SustainabilityBadges file extension to .jsx

--- components/SustainabilityBadges.jsx
+++ components/SustainabilityBadges.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import { Icons } from '../constants';
+import { Icons } from '../constants.js';
 
-const SustainabilityBadges: React.FC = () => {
+const SustainabilityBadges = () => {
   return (
     <div className="w-full bg-terra-100 py-6 border-b border-terra-200">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
